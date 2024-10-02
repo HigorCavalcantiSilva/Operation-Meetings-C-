566ad9dc5dc9667c9f34e921d75c9b8ab6c6ecd8
Higor Cavalcanti <higorsilva07.hs@gmail.com>
2024-10-02 19:17:24 -0400

    First Commit

---
+++ OperationMettings/Main.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace OperationMettings
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(754, 390);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Location = new System.Drawing.Point(12, 408);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(972, 309);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Location = new System.Drawing.Point(772, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(212, 144);
+            this.panel4.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MintCream;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(772, 162);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 240);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(996, 736);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1012, 775);
+            this.MinimumSize = new System.Drawing.Size(1012, 766);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Mettings Operation";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Shown += new System.EventHandler(this.Main_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ OperationMettings/Main.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using dotenv.net;
+
+namespace OperationMettings
+{
+    public partial class Main : Form
+    {
+        const int GWL_STYLE = -16; // Índice para pegar o estilo da janela
+        const int WS_CAPTION = 0xC00000; // Borda da janela com barra de título e botões
+        const int WS_THICKFRAME = 0x00040000; // Borda redimensionável
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private List<IntPtr> handleList;
+
+        public Main()
+        {
+            InitializeComponent();
+            handleList = new List<IntPtr>();
+            DotEnv.Load();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var handle in handleList)
+            {
+                try
+                {
+                    if (handle != null)
+                        SetParent(handle, IntPtr.Zero);
+                        SendMessage(handle, 0x0010, IntPtr.Zero, IntPtr.Zero);
+                } catch
+                {
+                    // Se cair aqui é porque o processo foi encerrado manualmente
+                }
+            }
+        }
+
+        private void InitializeObsStudio()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = Environment.GetEnvironmentVariable("PATH_OBS");
+            startInfo.FileName = $"{startInfo.WorkingDirectory}{Environment.GetEnvironmentVariable("EXECUTABLE_OBS")}";
+
+            OpenProcess(panel1, startInfo);
+        }
+
+        private void InitializeJWLibrary()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = $"shell:appsFolder\\{Environment.GetEnvironmentVariable("PATH_JW")}!App";
+
+            OpenProcessJW(startInfo);
+        }
+
+        private void InitializeOnlyT()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = Environment.GetEnvironmentVariable("PATH_ONLYT");
+            startInfo.FileName = $"{startInfo.WorkingDirectory}{Environment.GetEnvironmentVariable("EXECUTABLE_ONLYT")}";
+
+            OpenProcess(panel4, startInfo);
+        }
+
+        private void InitializeZoom()
+        {
+            string meetingId = Environment.GetEnvironmentVariable("MEETING_ID");
+            string password = Environment.GetEnvironmentVariable("PASSWORD");
+
+            Process process = new Process();
+            process.StartInfo.FileName = Environment.GetEnvironmentVariable("PATH_EXECUTABLE_ZOOM");
+            process.StartInfo.Arguments = $"--url=zoommtg://zoom.us/join?confno={meetingId}&pwd={password}";
+            process.Start();
+
+            IntPtr handle = FindWindow(null, "Zoom Reunião");
+            while (handle == IntPtr.Zero)
+            {
+                Thread.Sleep(1000);
+                handle = FindWindow(null, "Zoom Reunião");
+            }
+
+            Invoke((MethodInvoker)(() =>
+            {
+                int style = GetWindowLong(handle, GWL_STYLE);
+                SetWindowLong(handle, GWL_STYLE, style & ~WS_CAPTION & ~WS_THICKFRAME);
+
+                SetParent(handle, panel3.Handle);
+
+                MoveWindow(handle, 0, 0, panel3.Width, panel3.Height, true);
+
+                ShowWindow(handle, 5);
+
+                handleList.Add(handle);
+            }));
+        }
+
+        private void OpenProcess(Panel panel, ProcessStartInfo startInfo)
+        {
+            Process process = Process.Start(startInfo);
+            IntPtr handle;
+
+            handle = process.MainWindowHandle;
+
+            while (handle == IntPtr.Zero)
+            {
+                Thread.Sleep(1000);    
+                handle = process.MainWindowHandle;
+            }
+
+            Invoke((MethodInvoker)(() =>
+            {
+                int style = GetWindowLong(handle, GWL_STYLE);
+                SetWindowLong(handle, GWL_STYLE, style & ~WS_CAPTION & ~WS_THICKFRAME);
+
+                SetParent(handle, panel.Handle);
+
+                MoveWindow(handle, 0, 0, panel.Width, panel.Height, true);
+
+                ShowWindow(handle, 5);
+
+                handleList.Add(handle);
+            }));
+        }
+
+        private void OpenProcessJW(ProcessStartInfo startInfo)
+        {
+            IntPtr handle;
+            Process process = Process.Start(startInfo);
+
+            Thread.Sleep(4000);
+            handle = FindWindow(null, "JW Library");
+
+            Invoke((MethodInvoker)(() =>
+            {
+                MoveWindow(handle, Screen.PrimaryScreen.Bounds.Width - 375, 0, 300, 768, true);
+
+                handleList.Add(handle);
+            }));
+        }
+
+        private async void Main_Shown(object sender, EventArgs e)
+        {
+            // Executar os métodos simultaneamente usando Task.Run para cada método.
+            Task task1 = Task.Run(() => InitializeOnlyT());
+            Task task2 = Task.Run(() => InitializeObsStudio());
+            Task task3 = Task.Run(() => InitializeZoom());
+            Task task4 = Task.Run(() => InitializeJWLibrary());
+
+            // Aguarda todos os métodos terminarem.
+            await Task.WhenAll(task1, task2, task3, task4);
+        }
+    }
+}
